2bf7a499dfb83cb17cd4db8ec0628cdf6922fa24
George <16269580+Ge0rg3@users.noreply.github.com>
2020-09-18 13:00:25 +0000

    Add logging to start script & new stop script

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ PATH="/usr/local/bin:/usr/bin:/bin"
 # Get path of start.sh script
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # Start uwsgi server
-cd $SCRIPTPATH/backend && uwsgi --http 127.0.0.1:1337 --disable-logging --module app:app
+(cd $SCRIPTPATH/backend && uwsgi --http 127.0.0.1:1337 --disable-logging --module app:app) >>/log/admin.log 2>&1 &
